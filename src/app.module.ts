6851fd075b76e2c74a0701d1fc3aae001b012351
Kasperekx <piotr.kasperek92@gmail.com>
2023-05-23 19:59:18 +0200

    Delete unused stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/config/database/database.module';
 import { ApplicationModule } from './application/application.module';
 
 @Module({
   imports: [DatabaseModule, ApplicationModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
