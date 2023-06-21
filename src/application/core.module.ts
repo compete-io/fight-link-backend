9f5fc63bf583795c2b06cfeae209dd57ee2eee46
Kasperekx <piotr.kasperek92@gmail.com>
2023-06-21 13:00:02 +0200

    Adding user repository

--- src/application/core.module.ts
+++ src/application/core.module.ts
@@ -4,10 +4,11 @@ import { UserService } from 'src/domain/services/user.service';
 import { AuthService } from './services/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/infrastracture/entities/User/User.entity';
+import { UserRepository } from 'src/domain/repositories/user.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController],
-  providers: [UserService, AuthService],
+  providers: [UserService, AuthService, UserRepository],
 })
 export class CoreModule {}
